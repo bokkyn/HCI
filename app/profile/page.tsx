e6c0773045a9cee92a7b7e340244cabe3d12bcdd
bokkyn <b.g.bokki@gmail.com>
2026-02-11 15:06:04 +0100

    uređen profil section, bug fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -336,6 +336,7 @@ export default function ProfilePage() {
                   )}
                 </div>
               </div>
+              <div></div>
 
               {/* Moje ture Section */}
               <MyToursSection
